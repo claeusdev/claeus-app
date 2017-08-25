605b94c555cb476387711f066bfe841236dd44f4
Nana Adjei Manu <claeusdev@gmail.com>
2017-08-25 00:29:46 +0000

    Add more flash

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,12 @@
 module ApplicationHelper
-	
+	def flasherize(key)
+		case key.to_sym
+		when :error
+			"error"
+		when :success
+			"success"
+		else
+			"info"
+		end
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -42,5 +42,8 @@ Rails.application.routes.draw do
   
   root 'pages#home'
 
+  # Carts
+
+  post 'carts/add'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
